72e9296c8f7e1c690c93c64b1fd39d9c1e8c8c0b
smithjilks <smithjilks@gmail.com>
2022-04-14 19:29:44 +0300

    Add 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' This script contains a function add
+    that return a float sum of two floats '''
+
+
+def add(a: float, b: float) -> float:
+    ''' reurns sum of two float arguments as a float '''
+    return a + b
