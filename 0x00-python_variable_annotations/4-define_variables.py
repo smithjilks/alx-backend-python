6702c6d2295950e8c7cb43433de832e03b84e4cb
smithjilks <smithjilks@gmail.com>
2022-04-14 20:00:25 +0300

    Add 4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' This script defines and annotate the following variables
+with the specified values '''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
